96c753599f685d955fcc711b40796622b4124c58
Cihat Karaboğa <61988280+hesty@users.noreply.github.com>
2023-05-16 21:45:57 +0300

    add dial image

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:password_cracker_game/game_view.dart';
 
 void main() => runApp(const MyApp());
 
@@ -7,16 +8,9 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
       title: 'Password Cracker Game',
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Material App Bar'),
-        ),
-        body: const Center(
-          child: Text('Hello World'),
-        ),
-      ),
+      home: GameView(),
     );
   }
 }
